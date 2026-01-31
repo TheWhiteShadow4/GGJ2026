c2056eb81fef261a65b2c2eae5af3037992120ee
Asmolarski <andre.smolarski@outlook.de>
2026-01-31 05:11:10 +0100

    Fixed Resistance and Health Component

--- Assets/Scripts/MaskComponent.cs
+++ Assets/Scripts/MaskComponent.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class MaskComponent : MonoBehaviour, IDamageResistance
 {
-    [Header("Damage Resistance - Multiplier, (0 - 100% resistance)")]
-    [SerializeField] float _fire = 1f;
-    [SerializeField] float _water = 1f;
-    [SerializeField] float _air = 1f;
-    [SerializeField] float _earth = 1f;
+    [Header("HealthDamage = IncomingDamage / Resistance -> Resistance: (0, 100] -> HealthDamage [hoch, niedrig]")]
+    [SerializeField, Range(0.1f, 100f)] float _fireResistance = 1f;
+    [SerializeField, Range(0.1f, 100f)] float _waterResistance = 1f;
+    [SerializeField, Range(0.1f, 100f)] float _airResistance = 1f;
+    [SerializeField, Range(0.1f, 100f)] float _earthResistance = 1f;
 
     public float GetMultiplier(ElementType type)
         => type switch 
         {
-            ElementType.Fire => _air,
-            ElementType.Water => _water,
-            ElementType.Air => _air,
-            ElementType.Earth => _earth,
+            ElementType.Fire => _fireResistance,
+            ElementType.Water => _waterResistance,
+            ElementType.Air => _airResistance,
+            ElementType.Earth => _earthResistance,
             _ => 1f,
         };
 }
